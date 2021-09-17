81c302ab2a9c9276faf09a2d7a15cc72b5ccf2aa
gfuse <majidgeravand68@gmail.com>
2021-09-17 21:00:38 +0200

    add limit on VO odometry estimation

--- GPU_version/vio/src/frame_handler_mono.cpp
+++ GPU_version/vio/src/frame_handler_mono.cpp
@@ -224,7 +224,7 @@ FrameHandlerBase::UpdateResult FrameHandlerMono::processFrame()
             new_frame_->T_f_w_.se2().translation().y()-last_frame_->T_f_w_.se2().translation().y(),
             fabs(new_frame_->T_f_w_.pitch()-last_frame_->T_f_w_.pitch()));
 #endif
-    if((last_frame_->T_f_w_.se2().translation()-new_frame_->T_f_w_.se2().translation()).norm()>0.5 ||
+    if((last_frame_->T_f_w_.se2().translation()-new_frame_->T_f_w_.se2().translation()).norm()>0.005 ||
        fabs(new_frame_->T_f_w_.pitch()-last_frame_->T_f_w_.pitch())>M_PI_2 || sfba_n_edges_final < Config::qualityMinFts()){
         auto init_f= ukfPtr_.get_location();
         new_frame_->T_f_w_=init_f.second;
